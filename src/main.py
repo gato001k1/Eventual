400bc8e98f0341aa06fb035a4ca6ff7d2beb0461
gato001k1 <eduardoaltri520@gmail.com>
2025-12-08 09:20:48 -0500

    idk bro

--- src/main.py
+++ src/main.py
@@ -1,4 +1,15 @@
 import os
 import tkinter as tk
-import openpyxl
-import sendgrid
+#try:
+    #import openpyxl
+ #   import sendgrid
+
+
+
+#THING HERE
+
+window = tk.Tk()
+window.title("Eventual")
+window.geometry("900x600")
+window.configure(bg="#1E1E1E")
+window.mainloop()
