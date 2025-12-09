93e7b44d7747091d6c7c5d407fccb96c1aa2497b
gato001k1 <eduardoaltri520@gmail.com>
2025-12-09 09:09:55 -0500

    some date time thing

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import tkinter as tk
+from datetime import datetime
 #try:
     #import openpyxl
  #   import sendgrid
@@ -7,6 +8,26 @@ import tkinter as tk
 
 
 #THING HERE
+file = open("fifi.csv", "w")
+if not os.path.exist("fifi.csv"):
+    file = open("fifi.csv", "w")
+    file.write("Event Name, Date, Time, Description")
+else:
+    file = open("fifi.csv", "r")
+    
+def add_event(event_name, date, time, description):
+    with open("fifi.csv", "a") as file:
+        file.write(f"\n{event_name}, {date}, {time}, {description}")
+    
+def view_event():
+    with open("fifi.csv", "r") as file:
+        for cline, line in enumerate(file):
+            arr = line.split(", ")
+            if arr[1] == datetime.today() and arr[2] == datetime.now().strftime("%H:%M"):
+                dosomething()
+def dosomething():
+    print("KING FIFI")    
+
 
 window = tk.Tk()
 window.title("Eventual")
